4ad4d61ae65c009af5378b1503e182d512be7641
Amena Tousiath <tousiathaltaf@gmail.com>
2024-03-27 15:22:37 +0530

    some commit

--- env/app.py
+++ env/app.py
@@ -72,5 +72,5 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8000)
